71817748ec3bf26f8e942fed7a7039ddac98a9da
Ryuuko <applecat552000@hotmail.com>
2020-06-25 18:22:02 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (event) => {
     
     const location = search.value
 
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
 
     messageOne.textContent = 'loading~';
     messageTwo.textContent = '';
--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const forecast = require('./utils/forecast')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-
 const app = express()
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
@@ -123,6 +123,6 @@ app.get('*', (req, res) =>{
 })
 
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port, ()=>{
+    console.log('Server is up on port 3000.' + port)
 })
